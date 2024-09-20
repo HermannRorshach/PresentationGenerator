e5b180ca5f161d0675d4ed6bd9c3829842dfa858
Pavel <perevod-avtozavod@yandex.ru>
2024-09-20 04:27:33 +0300

    Могу добавлять изображения на страницу 9 презентации в нужное место с нужным размером

--- insert_image.py
+++ insert_image.py
@@ -1,7 +1,12 @@
 import fitz  # PyMuPDF
+from PIL import Image
+
+img = Image.open("page_8-image_4.png")
+img_width, img_height = img.size
+
 
 # Открываем существующий PDF
-pdf_path = "Первичный анализ ниши.pdf"
+pdf_path = "Первичный анализ ниши пустая презентация.pdf"
 doc = fitz.open(pdf_path)
 
 # Загружаем изображение
@@ -10,9 +15,10 @@ image = open(image_path, "rb").read()
 
 # Выбираем страницу (8-я страница имеет индекс 7)
 page = doc.load_page(8)
-
+x = 160
+y = 253
 # Определяем позицию для вставки (200 px сверху и слева)
-rect = fitz.Rect(100, 100, 100 + 904, 100 + 308)  # 100x100 - размеры изображения (можно подогнать под размеры картинки)
+rect = fitz.Rect(x, y, img_width * 0.62 + x, img_height * 0.62 + y)  # 100x100 - размеры изображения (можно подогнать под размеры картинки)
 
 # Вставляем изображение
 page.insert_image(rect, stream=image)
